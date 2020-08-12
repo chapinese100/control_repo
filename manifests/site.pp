f2761970a1b433ea7d7183460f4b09728d0a0154
chapinese100 <46344132+chapinese100@users.noreply.github.com>
2020-08-12 16:12:46 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,19 @@
 node default { 
+  file { '/rroot/README':
+   ensure  => file,
+   content => 'Ivan is an Infrastructure and DevOps Engineer',
+   owner   => 'root', 
  }
+}
 node 'puppetmaster.barrio.com' {
  include role::master_server
  }
+ 
+ node /^web/ {
+  include role::app_server
+ }
+ 
+ node /^db/ {
+  include role::db_server
+ }
+ 

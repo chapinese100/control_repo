19d27bb606d824757967427ae2d7e74be40def47
chapinese100 <46344132+chapinese100@users.noreply.github.com>
2020-08-12 22:27:45 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,4 +10,4 @@ class profile::ssh_server {
                 ensure  => present,
                 user   => 'root',
                 type   => 'ssh-rsa',
-                key    -> 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDeyKAqqaowY5552yFSMT2e/lYbYXM8euD3c8uw0vINhn2H6qosKj8Rb8gymr0JKKOnjlbplpmzQPR5Qto598Grkr1S27QtM8uCbJvSl+tKwD77Km9QiITEAmf/rwiYfSfBtZxf+d/87rmhcddhDO5piKuzj1JmyzrBXH6//otzjbIoJriMnOD4T4DWcLztI8c/w9bCz/uiJCG9TXSEtg3FwQ0x7u3kixWOT+QzwUe6XhCS35w4TPB912LW/78KTCyA+Wy2JkgJJr+iDxpsArGqMbxtO37ugFYSENA8AWm4mXZhFLRZa2kkz4dJbFdyN0nRpMlptCla4GxeMaUQP/6V',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDeyKAqqaowY5552yFSMT2e/lYbYXM8euD3c8uw0vINhn2H6qosKj8Rb8gymr0JKKOnjlbplpmzQPR5Qto598Grkr1S27QtM8uCbJvSl+tKwD77Km9QiITEAmf/rwiYfSfBtZxf+d/87rmhcddhDO5piKuzj1JmyzrBXH6//otzjbIoJriMnOD4T4DWcLztI8c/w9bCz/uiJCG9TXSEtg3FwQ0x7u3kixWOT+QzwUe6XhCS35w4TPB912LW/78KTCyA+Wy2JkgJJr+iDxpsArGqMbxtO37ugFYSENA8AWm4mXZhFLRZa2kkz4dJbFdyN0nRpMlptCla4GxeMaUQP/6V',
